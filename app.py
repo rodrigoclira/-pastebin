af97bad0c1cd8f53e867c8dc964b70d5457337a6
Rodrigo Lira <rodrigocliras@gmail.com>
2021-01-11 21:01:04 -0300

    creating repo

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+#import os 
+#from flask_sqlalchemy import SQLAlchemy
+from flask import session
+from waitress import serve
+
+
+app = Flask(__name__)
+
+def save_paste():
+    pass
+
+def load_paste():
+    pass
+
+def save_comment():
+    pass  
+
+
+def load_comment():  
+    pass
+
+def make_uid():
+    pass
+
+
+if __name__ == '__main__':
+
